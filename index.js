77e6feb01308fa590cc35fbfe3a45fe486acf8b7
Lara <laraellenh@gmail.com>
2022-01-23 19:32:23 -0500

    commit tree 6de00e1acba3027a5b08f71664f526d95b261ff1

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+
+function flipItAndReverseIt(word) {
+
+  // make string into an array with .split('')
+  // .reverse() that array, then .join to make the elements of the array a concatonated string in reverse of the original then use the flipit function with the parameter of word and if its equal to the reversed variable
+return word.split("").reverse().join("");
 }
+function isPalindrome(word) {
+  
+const newlyReversedWord = flipItAndReverseIt(word);
+
+return word === newlyReversedWord;
+}
+ 
 
 /* 
-  Add your pseudocode here
-*/
+in order to start, we need to take the string and make it into an array so we can call array methods on it. the method for that is .split () or we could use spread operator as well but can spread operator be used on a non object? lets gor .split
 
 /*
   Add written explanation of your solution here
